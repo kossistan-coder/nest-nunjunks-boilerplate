a9b3b89ce60cc1a4ad30d647cb9811bd75807092
kekpetsou <stan.ekpetsou@digital.gouv.tg>
2025-07-12 14:54:21 +0000

    [doing] (migrate entire angular dashboard in to nest project and redirect defaut home page to dashaboard page )

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('pages/index.njk')
-  getHello() {
-    return {};
+  getHello(@Req() req: Request, @Res() res: Response) {
+    return res.redirect('/workspace/dashboard/index');
   }
 }
